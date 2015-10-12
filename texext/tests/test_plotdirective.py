d7ec338d7dcff523ce8d237f58ebd6bc4f82a76f
Matthew Brett <matthew.brett@gmail.com>
2015-10-12 15:33:27 -0700

    RF: allow for earlier sphinx output in tests
    Earlier sphinx output had slightly different doctree.

--- texext/tests/test_plotdirective.py
+++ texext/tests/test_plotdirective.py
@@ -23,15 +23,23 @@ class TestPlotDirective(PageBuilder):
         doctree = pickle.loads(content)
         assert_equal(len(doctree.document), 1)
         para_strs = [str(p) for p in doctree.document[0]]
-        assert_equal(
-            para_strs,
-            ['<title>Plot directive with mathcode</title>',
-             '<paragraph>Some text</paragraph>',
-             '<literal_block highlight_args="{}" language="python" '
-             'linenos="False" xml:space="preserve">a = 101</literal_block>',
-             '<only expr="html"/>',
-             '<only expr="latex"/>',
-             '<only expr="texinfo"/>',
-             '<paragraph>More text</paragraph>',
-             '<displaymath docname="plot_and_math" label="None" '
-             'latex="101" nowrap="False"/>'])
+        assert_equal(para_strs[:2],
+                     ['<title>Plot directive with mathcode</title>',
+                      '<paragraph>Some text</paragraph>'])
+        if 'highlight_args' in para_strs[2]:  # sphinx 1.3.1 back to ?
+            assert_equal(
+                para_strs[2],
+                '<literal_block highlight_args="{}" language="python" '
+                'linenos="False" xml:space="preserve">a = 101</literal_block>')
+        else:  # sphinx 1.1.3 up until ?
+            assert_equal(
+                para_strs[2],
+                '<literal_block language="python" '
+                'linenos="False" xml:space="preserve">a = 101</literal_block>')
+        assert_equal(para_strs[3:],
+                     ['<only expr="html"/>',
+                      '<only expr="latex"/>',
+                      '<only expr="texinfo"/>',
+                      '<paragraph>More text</paragraph>',
+                      '<displaymath docname="plot_and_math" label="None" '
+                      'latex="101" nowrap="False"/>'])
