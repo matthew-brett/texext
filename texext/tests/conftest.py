2b85980173f95408114e6a3e042f4a3ae0164ffa
Matthew Brett <matthew.brett@gmail.com>
2018-07-31 02:30:14 +0100

    Another try at refactoring conftest.py

--- texext/tests/conftest.py
+++ texext/tests/conftest.py
@@ -3,4 +3,6 @@
 Otherwise it gets imported twice, raises an ImportMismatchError.
 """
 
-collect_ignore = ["conf.py"]
+from os.path import join as pjoin
+
+collect_ignore = [pjoin('tinypages', "conf.py")]
