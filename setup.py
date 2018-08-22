76ecd7c2da221df63157a4b358a79264bc821ee4
Matthew Brett <matthew.brett@gmail.com>
2018-08-22 15:06:14 +0100

    Drop Sphinx < 1.3.1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from distutils.core import setup
 import versioneer
 
 extra_setup_kwargs = ({} if 'setuptools' not in sys.modules else
-                      dict(install_requires=['six', 'sphinx>=1.1.3']))
+                      dict(install_requires=['six', 'sphinx>=1.3.1']))
 
 
 setup(name='texext',
